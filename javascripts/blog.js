f53ba3f09d9b6f9c726f86824a3225d231352b21
baocongchen <baocongchen@yahoo.com>
2015-07-15 15:31:13 +0700

    added blog

--- javascripts/blog.js
+++ javascripts/blog.js
@@ -3,12 +3,21 @@ $(function() {
     Parse.$ = jQuery;
  
     // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("G8cXsz7Z2PCMYbyKMRubHvk6BAgEYci2oYNMSUmm", "ovq6mqOtamFGGldOIKJXQLHLisXb3Z1sm1QpHObU");
- 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
+    Parse.initialize("G8cXsz7Z2PCMYbyKMRubHvk6BAgEYci2oYNMSUmm", "ovq6mqOtamFGGldOIKJXQLHLisXb3Z1sm1QpHObU"); 
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+      model: Blog
     });
- 
+
+    // Create a new instance of Blogs collection
+    var blogs = new Blogs();
+    
+    blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
+});
 });
